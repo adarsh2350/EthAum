6eaf6712975908d98649c86aefa243ca27a918e3
Adarsh kumar <adarsh2350i@gmail.com>
2024-06-15 18:15:56 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,47 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 app = express()
+dotenv.config({path:'.env'})
+
+const db = process.env.DATABASE
+const PORT = process.env.PORT
+
+mongoose.connect(db).then(() => {
+    console.log('database connected')
+}).catch(() => {
+    console.log("can't connect to database")
+})
 
 app.get('/', (req, res) => {
     res.send('Home page');
 })
 
-app.listen(8000, () => {
+app.get('/login', (req, res) => {
+    res.send('Login page');
+})
+
+app.get('/signuo', (req, res) => {
+    res.send('signup page');
+})
+
+app.get('/profile', (req, res) => {
+    res.send('profile page');
+})
+
+app.get('/cart', (req, res) => {
+    res.send('Cart');
+})
+
+app.get('/marketplace', (req, res) => {
+    res.send('Marketplace');
+})
+
+app.get('*', (req, res) => {
+    res.send('ERROR');
+})
+
+
+app.listen(PORT, () => {
     console.log('server is listening')
 })
